7070fdd895a51e33249120520fcef7188a39a29d
Jean-Philippe McCluskey <jeanphilippe.mccluskey@gmail.com>
2019-02-17 17:47:23 -0500

    making anagram problem HackerRank

---
+++ Hanker Ranker/Making Anagrams/makingAnagrams.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the makeAnagram function below.
+    static int makeAnagram(String a, String b) {
+        int[] lettercounts = new int[26];
+        for(char c : a.toCharArray()){
+            lettercounts[c-'a']++;
+        }
+        for(char c : b.toCharArray()){
+            lettercounts[c-'a']--;
+        }
+        int result = 0;
+        for(int i : lettercounts){
+            result += Math.abs(i);
+        }
+        return result;  
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String a = scanner.nextLine();
+
+        String b = scanner.nextLine();
+
+        int res = makeAnagram(a, b);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
